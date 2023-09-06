b2b941f50f65b61891dcb8d723729fda6a436449
Hannah Del Real <hdelreal@my365.bellevue.edu>
2023-09-05 18:09:46 -0700

    adding comment

--- week-5/in-n-out-booksp1/src/app/book.interface.ts
+++ week-5/in-n-out-booksp1/src/app/book.interface.ts
@@ -1,3 +1,10 @@
+/**
+; Title: app.interface.ts
+; Author: Professor Krasso
+; Date: 09/04/2023
+; Modified By: Hannah Del Real
+; Description: Book Interface
+*/
 export interface IBook {
   isbn: string;
   title: string;
